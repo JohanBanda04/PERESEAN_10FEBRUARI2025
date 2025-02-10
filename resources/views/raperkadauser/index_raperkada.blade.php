64622cba3aa2acd47a77cead3776bcb2255d04cd
JohanBanda04 <banda04.johan@gmail.com>
2025-02-10 15:47:25 +0800

    PERESEANLARAVEL_10FEBRUARI2025_II

--- resources/views/raperkadauser/index_raperkada.blade.php
+++ resources/views/raperkadauser/index_raperkada.blade.php
@@ -244,6 +244,84 @@
                                            margin: 0 auto; overflow-x: auto; white-space: nowrap;">{{ $d->nama_perancang }}</td>
                                                 <td style=" max-width: -moz-fit-content; max-width: fit-content;
                                            margin: 0 auto; overflow-x: auto; white-space: nowrap;">{{ strtoupper($d->jenis_dokumen) }}</td>
+                                                <td style=" max-width: -moz-fit-content; max-width: fit-content;
+                                           margin: 0 auto; overflow-x: auto; white-space: nowrap;">
+                                                    <div class="btn-group">
+                                                        {{--edit disini--}}
+
+                                                        <a href="#" class="tampilkandetail btn btn-warning btn-sm"
+                                                           id_berita="{{--{{ $d->id_berita }}--}}"
+                                                           style="margin-right: 5px; border-radius: 5px; background-color: dodgerblue" title="Lihat Detail?">
+                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                 class="icon icon-tabler icon-tabler-file-description"
+                                                                 width="24" height="24" viewBox="0 0 24 24"
+                                                                 stroke-width="1.5" stroke="currentColor" fill="none"
+                                                                 stroke-linecap="round" stroke-linejoin="round">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                <path d="M14 3v4a1 1 0 0 0 1 1h4"/>
+                                                                <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"/>
+                                                                <path d="M9 17h6"/>
+                                                                <path d="M9 13h6"/>
+                                                            </svg>
+                                                        </a>
+
+
+
+                                                        <form action="{{--{{ route('deleteberita',[$d->id_berita,$d->kode_satker]) }}--}}"
+                                                              method="post"
+                                                              style="margin-left: 5px">
+                                                            @csrf
+                                                            {{--                                                            @method('DELETE')--}}
+                                                            <a style="height: 20px; border-radius: 5px;"
+                                                               class="btn btn-danger btn-sm delete-confirm-berita"
+                                                               title="Hapus Berita?">
+                                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                                     class="icon icon-tabler icon-tabler-trash-filled"
+                                                                     width="24" height="24" viewBox="0 0 24 24"
+                                                                     stroke-width="2" stroke="currentColor" fill="none"
+                                                                     stroke-linecap="round" stroke-linejoin="round">
+                                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                    <path d="M20 6a1 1 0 0 1 .117 1.993l-.117 .007h-.081l-.919 11a3 3 0 0 1 -2.824 2.995l-.176 .005h-8c-1.598 0 -2.904 -1.249 -2.992 -2.75l-.005 -.167l-.923 -11.083h-.08a1 1 0 0 1 -.117 -1.993l.117 -.007h16z"
+                                                                          stroke-width="0" fill="currentColor"/>
+                                                                    <path d="M14 2a2 2 0 0 1 2 2a1 1 0 0 1 -1.993 .117l-.007 -.117h-4l-.007 .117a1 1 0 0 1 -1.993 -.117a2 2 0 0 1 1.85 -1.995l.15 -.005h4z"
+                                                                          stroke-width="0" fill="currentColor"/>
+                                                                </svg>
+                                                            </a>
+                                                        </form>
+
+                                                        <a href="#" class="editberita btn-sm"
+                                                           style="background-color: #3ebc16; border-radius: 5px;
+                                                           color: white; margin-left: 9px;"
+                                                           id_berita="{{--{{ $d->id_berita }}--}}" title="Edited By Perancang?">
+                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                 class="icon icon-tabler icon-tabler-edit" width="24"
+                                                                 height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                                 stroke="currentColor" fill="none"
+                                                                 stroke-linecap="round" stroke-linejoin="round">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"/>
+                                                                <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"/>
+                                                                <path d="M16 5l3 3"/>
+                                                            </svg>
+                                                        </a>
+
+                                                        <a href="#" class="editberita btn-sm"
+                                                           style="background-color: #1e35bc; border-radius: 5px;
+                                                           color: white; margin-left: 8px;"
+                                                           id_berita="{{--{{ $d->id_berita }}--}}" title="Edited By Pemda?">
+                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                 class="icon icon-tabler icon-tabler-edit" width="24"
+                                                                 height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                                 stroke="currentColor" fill="none"
+                                                                 stroke-linecap="round" stroke-linejoin="round">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"/>
+                                                                <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"/>
+                                                                <path d="M16 5l3 3"/>
+                                                            </svg>
+                                                        </a>
+                                                    </div>
+                                                </td>
 
                                             </tr>
                                         @endforeach
